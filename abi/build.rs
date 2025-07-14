ff4dd8eb5185a2f4f8902ec6a0f077cd6b325e06
wzf <1490216271@qq.com>
2025-07-14 11:45:28 +0800

    protobuf

---
+++ abi/build.rs
@@ -0,0 +1,17 @@
+
+fn main(){
+
+    tonic_build::configure()
+        .out_dir("src/pb")
+        .compile(&["protos/reservation.proto"],&["protos"])
+        .unwrap();
+
+    Command::new("cargo")
+        .args(&["fmt"])
+        .current_dir("src/pb")
+        .output()
+        .unwrap();
+
+
+
+}
---
+++ abi/src/pb/mod.rs
@@ -0,0 +1,5 @@
+
+#[allow(clippy::all,non_camel_case_types)]
+mod reservation;
+
+pub use reservation::*;
